1548532e396c67465e46f3b0cf5e710871ae83bf
adamflitney <adam.flitney@gmail.com>
2019-12-31 11:06:42 +0000

    added shop page with collection previews

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
 import HomePage from './pages/homepage'
+import ShopPage from './pages/shop/shop'
 
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <HomePage />
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
+    </div>
   );
 }
 

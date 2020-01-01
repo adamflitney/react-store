d9599035dddc553b5bc7c54bf6be2b4ee3df9951
adamflitney <adam.flitney@gmail.com>
2020-01-01 15:02:00 +0000

    added redirect if user signed in

--- src/App.js
+++ src/App.js
@@ -1,40 +1,36 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
 
-import HomePage from './pages/homepage/homepage'
-import ShopPage from './pages/shop/shop'
-import SignInUpPage from './pages/sign-in-up/sign-in-up'
-import Header from './components/header/header'
-import { auth, createUserProfileDocument } from './firebase/firebase.utils'
-import { setCurrentUser } from './redux/user/user.actions'
+import HomePage from "./pages/homepage/homepage";
+import ShopPage from "./pages/shop/shop";
+import SignInUpPage from "./pages/sign-in-up/sign-in-up";
+import Header from "./components/header/header";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import { setCurrentUser } from "./redux/user/user.actions";
 
-import './App.css';
-
+import "./App.css";
 
 class App extends React.Component {
-
-  unsubscribeFromAuth = null
+  unsubscribeFromAuth = null;
 
   componentDidMount() {
     const { setCurrentUser } = this.props;
 
-    this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
-      if(userAuth) {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapshot => {
           setCurrentUser({
-              id: snapshot.id,
-              ...snapshot.data()
-          })
-        })
-
+            id: snapshot.id,
+            ...snapshot.data()
+          });
+        });
       } else {
-        setCurrentUser(userAuth)
+        setCurrentUser(userAuth);
       }
-      
-    })
+    });
   }
 
   componentWillUnmount() {
@@ -46,18 +42,27 @@ class App extends React.Component {
       <div>
         <Header />
         <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInUpPage} />
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              this.props.currentUser ? <Redirect to="/" /> : <SignInUpPage />
+            }
+          />
         </Switch>
       </div>
     );
-    
   }
 }
+
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
